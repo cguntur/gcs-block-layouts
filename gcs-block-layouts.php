f47873ba1b60b42c9a1bb7ee42a53753dd18a490
Chandrika Guntur <cguntur@gcsdesign.com>
2021-01-12 16:57:14 -0800

    First Commit

---
+++ gcs-block-layouts.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: GCS Block Layouts
+Plugin URI: http://www.gcsdesign.com
+Description: Collection of page layouts for the WordPress Gutenberg editor
+Version: 1.0
+Author: Chandrika Sista
+Author URI: https://www.gcsdesign.com/
+Text Domain: gcs_bl
+*/
+?>
